0b4f4a56366260f1b19e88a5d9e9955f1b340e11
AnanasekE <janeksambor@gmail.com>
2022-10-10 15:49:00 +0200

    init

---
+++ src/views/Admin/Admin.js
@@ -0,0 +1,9 @@
+import Header from '../../components/Header';
+
+const Admin = () => {
+    return (
+        <Header title='Admin' />
+    )
+}
+
+export default Admin;
---
+++ src/views/Website/Website.js
@@ -0,0 +1,9 @@
+import Header from '../../components/Header';
+
+const Website = () => {
+    return (
+        <Header title='Website' />
+    )
+}
+
+export default Website;
